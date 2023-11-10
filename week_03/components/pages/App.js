c196615beb232c5fed10165a8d7cb6b62895e413
ParkDohye <doheopark@hanyang.ac.kr>
2023-11-10 15:51:59 +0900

    1

--- week_03/components/pages/App.js
+++ week_03/components/pages/App.js
@@ -3,7 +3,7 @@ import Movie from "../Movies/Movie";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">1
       <div className="app-container">
         {movies.results.slice(0, 1).map((item) => {
           return <Movie movie={item} />;
